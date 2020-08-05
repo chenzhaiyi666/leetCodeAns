b5031ca3231ce5af77be20a455829f0107b40a25
Chen Zhaiyi <zhaiyi.chen@ipinyou.com>
2020-08-05 11:58:35 +0800

    test

--- ans/src/main/java/com/chen/leet/ans/AnsApplication.java
+++ ans/src/main/java/com/chen/leet/ans/AnsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//test_ti_jiao
 public class AnsApplication {
 
 	public static void main(String[] args) {
